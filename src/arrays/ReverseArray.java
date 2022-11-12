f6e88851d6065544f90bfb8da6a3709a0d80aa8e
v.gurguriev <v.gurguriev@gmail.com>
2022-11-12 12:51:52 +0200

    Initial commit.

---
+++ src/arrays/ReverseArray.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int[] arr = fillArray(input);
+
+        reverseArr(arr);
+        printArr(arr);
+    }
+
+    private static void reverseArr(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    private static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    private static int[] fillArray(String input) {
+
+        String[] tokens = input.split("\\s+");
+
+        int[] arr = new int[tokens.length];
+
+        for (int i = 0; i < tokens.length; i++) {
+            arr[i] = Integer.parseInt(tokens[i]);
+        }
+
+        return arr;
+    }
+}
