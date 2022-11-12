dde96519f8d36870fa7b62e2f23af850d3c9647c
vgurguriev <v.gurguriev@gmail.com>
2022-11-12 13:31:32 +0200

    Initial commit

--- src/arrays/SecondMinValue.java
+++ src/arrays/SecondMinValue.java
@@ -1,7 +1,45 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class SecondMinValue {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        int[] arr = fillArray(input);
+
+        int secondMin = findSecondMinNumber(arr);
+
+        System.out.println(secondMin);
+    }
+
+    private static int findSecondMinNumber(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int j : arr) {
+            if (j < min) {
+                secondMin = min;
+                min = j;
+            } else if (j < secondMin && j != min) {
+                secondMin = j;
+            }
+        }
+        return secondMin;
+    }
+
+    private static int[] fillArray(String input) {
+
+        String[] tokens = input.split("\\s+");
+
+        int[] arr = new int[tokens.length];
+
+        for (int i = 0; i < tokens.length; i++) {
+            arr[i] = Integer.parseInt(tokens[i]);
+        }
+
+        return arr;
     }
 }
