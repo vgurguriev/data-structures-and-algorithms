fd13274750f67e761c22965cb91c768407dc3811
vgurguriev <v.gurguriev@gmail.com>
2022-11-13 13:39:33 +0200

    Initial commit

---
+++ src/arrays/MoveZeroesToTheEnd.java
@@ -0,0 +1,28 @@
+package arrays;
+
+public class MoveZeroesToTheEnd {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 0, 4, 12};
+
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0 && arr[j] == 0) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+            if (arr[j] != 0) {
+                j++;
+            }
+        }
+
+        printArr(arr);
+    }
+
+    private static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
