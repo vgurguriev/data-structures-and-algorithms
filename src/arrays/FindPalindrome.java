ce0e5f9a59db3226f90b35a130cb73d6ff6746c0
vgurguriev <v.gurguriev@gmail.com>
2022-11-15 00:37:35 +0200

    Initial commit

---
+++ src/arrays/FindPalindrome.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FindPalindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        char[] arr = input.toCharArray();
+        boolean result = isPalindrome(arr);
+
+        System.out.println(result);
+    }
+
+    private static boolean isPalindrome(char[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
