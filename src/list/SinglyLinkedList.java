2b733c81871851a685f04dbf81956e686157efde
vgurguriev <v.gurguriev@gmail.com>
2022-11-15 01:20:52 +0200

    Initial commit

--- src/list/SinglyLinkedList.java
+++ src/list/SinglyLinkedList.java
@@ -12,4 +12,21 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList(); // head is null
+        sll.head = new ListNode(1);
+
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        // connect them to form a chain
+
+        sll.head.next = second; // 1 --> 2
+        second.next = third; // 1 --> 2 --> 3
+        third.next = fourth; // 1 --> 2 --> 3 --> 4 --> null
+
+        System.out.println(sll);
+    }
 }
