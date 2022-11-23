0ab6bc68dd7b7e83c2a26b906b618efdfe239ddd
vgurguriev <v.gurguriev@gmail.com>
2022-11-23 23:30:45 +0200

    Initial commit

--- src/list/SinglyLinkedList.java
+++ src/list/SinglyLinkedList.java
@@ -33,6 +33,22 @@ public class SinglyLinkedList {
 
     }
 
+    public ListNode getMiddleNode() {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while (fastPtr != null && fastPtr.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+
+        return slowPtr;
+    }
+
     public ListNode reverse(ListNode head) {
         if (head == null) {
             return null;
