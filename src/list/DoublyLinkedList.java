df3adc0a2b408092f754cd34b5bb5dace700515d
vgurguriev <v.gurguriev@gmail.com>
2022-12-12 11:02:58 +0200

    Created DoublyLinkedList class

---
+++ src/list/DoublyLinkedList.java
@@ -0,0 +1,32 @@
+package list;
+
+public class DoublyLinkedList {
+
+    private ListNode head;
+    private ListNode tail;
+    private int length;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    private class ListNode {
+        private int data;
+        private ListNode previous;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+
+        public boolean isEmpty() {
+            return length == 0;
+        }
+
+        public int length() {
+            return length;
+        }
+    }
+}
