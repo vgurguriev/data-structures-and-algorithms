2e74dae9011d3e1354f0a504fe6b7d3721b7c115
vgurguriev <v.gurguriev@gmail.com>
2022-12-14 14:44:45 +0200

    Added CircularSinglyLinkedList

---
+++ src/list/CircularSinglyLinkedList.java
@@ -0,0 +1,48 @@
+package list;
+
+public class CircularSinglyLinkedList {
+    private ListNode last;
+    private int length;
+
+    private class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public CircularSinglyLinkedList() {
+        last = null;
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    public void createCircularLinkedList() {
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+
+        last = fourth;
+
+    }
+
+    public static void main(String[] args) {
+        CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+        csll.createCircularLinkedList();
+    }
+}
