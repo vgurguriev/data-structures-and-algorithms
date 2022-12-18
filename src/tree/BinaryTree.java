37b805aec0feed641a23887ec4701e02e7bbe7f6
vgurguriev <v.gurguriev@gmail.com>
2022-12-18 13:45:15 +0200

    Added preOrder()

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -14,17 +14,32 @@ public class BinaryTree {
     private TreeNode root;
 
     public void createBinaryTree() {
-        TreeNode first = new TreeNode(1);
+        TreeNode first = new TreeNode(9);
         TreeNode second = new TreeNode(2);
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
-        TreeNode fifth = new TreeNode(5);
 
         root = first;
 
         first.left = second;
         first.right = third;
         second.left = fourth;
-        second.right = fifth;
+    }
+
+    public void preOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.createBinaryTree();
+        binaryTree.preOrder(binaryTree.root);
     }
 }
